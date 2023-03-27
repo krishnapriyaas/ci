9b8a40ae9258343cc1416d86496a9af143558bc2
krishnapriyaas <krishnapriyaas1997@gmail.com>
2023-03-27 15:35:27 +0530

    Lesson 12: Transfering data to views

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -6,14 +6,18 @@ class Users extends CI_Controller {
     public function show()
 	{
 		
-		$this->load->model('user_model');
-        $result = $this->user_model->get_users();
-        foreach ($result as $object)
-        {
-            echo $object->id;
-            echo $object->username;
-            echo $object->password;
-        }
+		// $this->load->model('user_model');
+        $data['results'] = $this->user_model->get_users();
+        $this->load->view('user_view',$data);
+        // foreach ($result as $object)
+        // {
+        //     echo $object->id;
+        //     echo $object->username;
+        //     echo $object->password;
+        // }
+
+
+
         
 	}
 
---
+++ application/views/user_view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php
+
+foreach ($results as $object)
+{
+echo $object->username ."</br>";
+}
+
+
+?>
+
+
+
+
+</body>
+</html>
