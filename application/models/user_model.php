e3ad28a1f3474974a063b954b50be987418e42e7
krishnapriyaas <krishnapriyaas1997@gmail.com>
2023-03-27 13:06:19 +0530

    Lesson 10: Autoloading model

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -3,9 +3,15 @@ class user_model extends CI_Model
 {
 public function get_users()
 {
+        // $config['localhost']="localhost";
+        // $config['username']="root";
+        // $config['password']="";
+        // $config['database']="errand_db";
 
-
-
+        // $connection=$this->load->database($config) 
+        
+         $query= $this->db>get('users');
+         return $query->result();
 }
 
 
