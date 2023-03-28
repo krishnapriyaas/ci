c7ec0e6c21e5bec47efe1d2c7a31c8d0b9ed270e
krishnapriyaas <krishnapriyaas1997@gmail.com>
2023-03-28 17:44:25 +0530

    Lesson 28: Form processing methods

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -3,56 +3,64 @@
 
 class Users extends CI_Controller {
 
-    public function show($user_id)
-	{
+    // public function show($user_id)
+	// {
 		
-		$this->db->where(['id' => $user_id,'krishna']);
-        $data['results'] = $this->user_model->get_users($user_id);
-        $this->load->view('user_view',$data);
+	// 	$this->db->where(['id' => $user_id,'krishna']);
+    //     $data['results'] = $this->user_model->get_users($user_id);
+    //     $this->load->view('user_view',$data);
         
-		// $this->load->model('user_model');
-        // foreach ($result as $object)
-        // {
-        //     echo $object->id;
-        //     echo $object->username;
-        //     echo $object->password;
-        // }
+	// 	// $this->load->model('user_model');
+    //     // foreach ($result as $object)
+    //     // {
+    //     //     echo $object->id;
+    //     //     echo $object->username;
+    //     //     echo $object->password;
+    //     // }
 
 
 
         
-	}
-    public function insert()
-    {
-        $username='peter';
-        $password="1234";
-        $this->user_model->create_users([
-        'username'=> $username,
-        'password'=>$password
+	// }
+    // public function insert()
+    // {
+    //     $username='peter';
+    //     $password="1234";
+    //     $this->user_model->create_users([
+    //     'username'=> $username,
+    //     'password'=>$password
 
 
-        ]);
+    //     ]);
 
-    }
-    public function update()
-    {
-        $id=1;
-        $username='meera';
-        $password="12546844";
-        $this->user_model->update_users([
-        'username'=> $username,
-        'password'=>$password
+    // }
+    // public function update()
+    // {
+    //     $id=1;
+    //     $username='meera';
+    //     $password="12546844";
+    //     $this->user_model->update_users([
+    //     'username'=> $username,
+    //     'password'=>$password
 
 
-        ],$id);
+    //     ],$id);
 
-    }
-    public function delete()
-    {
-        $id=1;
+    // }
+    // public function delete()
+    // {
+    //     $id=1;
        
-        $this->user_model->delete_users($id);
+    //     $this->user_model->delete_users($id);
 
+    // }
+ 
+
+
+    public function login()
+    {
+        echo $this->input->post('username');
+        //echo $_POST['username'];
     }
 
 }
--- application/views/users/login_view.php
+++ application/views/users/login_view.php
@@ -1,24 +1,50 @@
 <h4>Login Form</h4>
-<?php $attribute=array('id'=>'login_form','class'=>'form_horizontal'); ?>
-<?php echo form_open('users/login_view',$attribute); ?>
+<?php $attribute = array('id' => 'login_form' , 'class' => 'form_horizontal'); ?>
+<?php echo form_open('users/login',$attribute);?>
 
 <div class="form-group">
-    <?php echo form_label('username'); ?>
-<?php    
-$data=array(
 
-    'class' => 'form-control',
+<?php echo form_label('Username');?>
+
+
+<?php 
+
+    $data = array(
+    'class' => 'form_control',
     'name' => 'username',
-    'placeholder' => 'Enter User Name'
-   
-);
-
-
+    'placeholder' => 'Enter username'
+    );
 ?>
-<?php echo form_input($data); ?>
+<?php echo form_input($data);?>
 </div>
 
+<div class="form-group">
+
+<?php echo form_label('Password');?>
 
 
-<?php  echo form_close();?>
- 
+<?php 
+
+    $data = array(
+    'class' => 'form_control',
+    'name' => 'password',
+    'placeholder' => 'Enter password'
+    );
+?>
+<?php echo form_password($data);?>
+</div>
+
+<div class="form-group">
+
+<?php 
+
+    $data = array(
+    'class' => 'btn btn-primary',
+    'name' => 'submit',
+    'value' => 'Login'
+    );
+?>
+<?php echo form_submit($data);?>
+</div>
+
+<?php echo form_close();?>
