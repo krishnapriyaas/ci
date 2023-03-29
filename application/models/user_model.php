38325d3d871c01fcd9af9dd4ba8dd280b682ba74
krishnapriyaas <krishnapriyaas1997@gmail.com>
2023-03-29 16:11:14 +0530

    lesson 36: added in user model

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,46 +1,69 @@
 <?php
 class user_model extends CI_Model
 {
-        public function get_users($user_id,$user_name)
+        // public function get_users($user_id,$user_name)
+        // {
+        //         $this->db->where(['id'=> $user_id,
+        //         'username' =>$user_name
+
+        //         ]);
+
+        //         $query= $this->db->get('users');
+        //         return $query->result();
+
+
+        //         // $config['localhost']="localhost";
+        //         // $config['username']="root";
+        //         // $config['password']="";
+        //         // $config['database']="errand_db";
+
+        //         // $connection=$this->load->database($config) 
+        
+        //         //  $query= $this->db->get('users');
+        //         //  return $query->result();
+        //         $query=$this->db->query("SELECT * FROM  users");
+        //         //return $query->num_fields();//this will give me the columns  count
+        //         return $query->num_rows();//this will give me the rows  count
+        // }
+
+
+        // public function create_users($data){
+        //         $this->db->insert('users',$data);
+        // }
+
+
+        // public function update_users($data,$id){
+        //         $this->db->where(['id'=>$id]);
+        //         $this->db->update('users',$data);
+        // }
+        // public function delete_users($id){
+        //         $this->db->where(['id'=>$id]);
+        //         $this->db->delete('users');
+        // }
+   
+     public function login_users($username,$password)   
         {
-                $this->db->where(['id'=> $user_id,
-                'username' =>$user_name
 
-                ]);
+           $this->db->where('username',$username);
+           $this->db->where('password',$password);
+           $result=$this->db->get('users');
+           if($result->num_row()==1)
+           {
+             return $result->row(0)->id;
+           }
+           else{
 
-                $query= $this->db->get('users');
-                return $query->result();
+                return false;
+           }
 
-
-                // $config['localhost']="localhost";
-                // $config['username']="root";
-                // $config['password']="";
-                // $config['database']="errand_db";
-
-                // $connection=$this->load->database($config) 
-        
-                //  $query= $this->db->get('users');
-                //  return $query->result();
-                $query=$this->db->query("SELECT * FROM  users");
-                //return $query->num_fields();//this will give me the columns  count
-                return $query->num_rows();//this will give me the rows  count
         }
 
 
-        public function create_users($data){
-                $this->db->insert('users',$data);
-        }
 
 
-        public function update_users($data,$id){
-                $this->db->where(['id'=>$id]);
-                $this->db->update('users',$data);
-        }
-        public function delete_users($id){
-                $this->db->where(['id'=>$id]);
-                $this->db->delete('users');
-        }
-        
+
+
+
 }
 
 
