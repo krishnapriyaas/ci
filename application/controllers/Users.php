e0edb56b0e1d37acf258cd65b5e1f1aebb749cb1
krishnapriyaas <krishnapriyaas1997@gmail.com>
2023-03-30 17:31:27 +0530

    lesson 48:Validations and solutions

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,7 +1,25 @@
 <?php
 class Users extends CI_Controller{
+  public function register(){
 
-public function login(){
+     $this->form_validation->set_rules('first_name', 'First Name', 'trim|required|min_length[3]');
+     $this->form_validation->set_rules('last_name', 'Last Name', 'trim|required|min_length[3]');
+     $this->form_validation->set_rules('email', 'Email', 'trim|required|min_length[3]');
+     $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[3]');
+     $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[3]');
+     $this->form_validation->set_rules('confirm_password', 'Confirm Password', 'trim|required|min_length[3]|matches[password]');
+   
+
+     if ($this->form_validation->run() == FALSE) {
+      $data['main_view']='Users/register_view';
+      $this->load->view('layouts/main',$data);
+     
+    }else{
+      redirect('');
+    
+    }
+  }
+  public function login(){
 
      $this->form_validation->set_rules('username', 'Username', 'trim|required|min_length[3]');
      $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[3]');
@@ -9,47 +27,45 @@ public function login(){
    
 
      if ($this->form_validation->run() == FALSE) {
-     $data = array(
+        $data = array(
 
-      'errors' => validation_errors(),
-      'main_view'=>'home_view'
+        'errors' => validation_errors(),
+        'main_view'=>'home_view'
 
-  );
- //print_r($data);
-  $this->session->set_flashdata($data);
-  //redirect('home');
-  $this->load->view('layouts/main',$data);
-}
-else {
-  $username = $this->input->post('username'); 
-  $password = $this->input->post('password');
+      );
+ 
+      $this->session->set_flashdata($data);
+  
+      $this->load->view('layouts/main',$data);
+    }
+    else {
+      $username = $this->input->post('username'); 
+      $password = $this->input->post('password');
 
-  $user_id = $this->user_model->login_user($username,$password);
-     if( $user_id ){
-      $user_data = array(
+       $user_id = $this->user_model->login_user($username,$password);
+      if( $user_id ){
+        $user_data = array(
 
           'user_id' => $user_id,
           'username' => $username,
           'logged_in' => true
 
-      );
+        );
 
-      $this->session->set_userdata($user_data);
-      $this->session->set_flashdata('login_success','you are now loged in');
+        $this->session->set_userdata($user_data);
+        $this->session->set_flashdata('login_success','you are now loged in');
 
-      $data['main_view'] = "admin_view";
-		  $this->load->view('layouts/main',$data);
-     // redirect('home/index');
-     }
-     else{
+        $data['main_view'] = "admin_view";
+		    $this->load->view('layouts/main',$data);
+        // redirect('home/index');
+      }
+      else{
       $this->session->set_flashdata('login_failed','sorry you are not loged in');
       redirect('home/index');
-     }
-}
-  //echo $this->input->post('username');
-  //echo " this works";
-  //echo $_POST['username'];
-}
+      }
+    }
+    
+    }
 
 
 
@@ -105,12 +121,12 @@ public function delete()
   $this->user_model->delete_users($id);
   
 }*/
-public function logout(){
+  public function logout(){
 
   $this->session->sess_destroy();
   redirect('home/index');
 
-}
+  }
 
 }
 ?>
