f71a505bdd68c0f7918e426bfc4d3629c6840f7f
krishnapriyaas <krishnapriyaas1997@gmail.com>
2023-03-31 13:10:24 +0530

    lesson :Projects home page

---
+++ application/controllers/projects.php
@@ -0,0 +1,15 @@
+<?php
+
+class Projects extends CI_Controller {
+
+    public function index()
+	{
+		$data['main_view'] = "projects/index";
+		$this->load->view('layouts/main',$data);
+	}
+
+
+  
+}
+
+?>
